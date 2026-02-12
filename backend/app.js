4d1b34c212801e7639739a7e2a241fec6d42c0d1
Cristian Contreras <contreras.cristian994@gmail.com>
2026-02-11 22:24:32 -0500

    deploy path added

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 import express from "express"
 import messagesRouter from "./routes/messagesRouter.js";
 import cors from "cors"
+import path from 'path';
+
 const app = express();
+const __dirname = path.resolve();
 app.use(cors());
 app.use(express.json());
 
@@ -17,6 +20,14 @@ app.use("/messages", messagesRouter);
 app.get("/", (req, res) => {
     res.send("Hello world");
 })
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 const PORT = 3000;
 
