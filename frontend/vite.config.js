8e8b909361afd961597944228f1b0aa8c8763315
Cristian Contreras <contreras.cristian994@gmail.com>
2026-02-11 22:58:40 -0500

    build0.4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  server: {
+    post: 3000,
+    proxy: {
+      "/messages": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
+});
